d51e859e1d5c8e9c70f1311f9ed47cd8aafab199
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2023-05-06 00:31:27 +0400

    + basic http client - unnecessary things

--- src/main/scala/ru/eleventh/anklish/Main.scala
+++ src/main/scala/ru/eleventh/anklish/Main.scala
@@ -1,7 +1,25 @@
 package ru.eleventh.anklish
 
+
 import cats.effect.{IO, IOApp}
+import cats.effect.unsafe.IORuntime
+import org.http4s.client.{Client, JavaNetClientBuilder}
+import org.http4s.implicits._
 
 object Main extends IOApp.Simple {
-  def run: IO[Unit] = AnklishServer.run
+
+  private val URL = uri"https://api.dictionaryapi.dev/api/v2/entries/en"
+  private val client: Client[IO] = JavaNetClientBuilder[IO].create
+
+  def run: IO[Unit] = {
+    val res = client.expect[String](s"$URL/hello").onError(e => {
+      println(e.getMessage)
+      IO(())
+    }).unsafeRunSync()(runtime)
+
+    IO(res).map { body =>
+      println(body)
+      ()
+    }
+  }
 }
