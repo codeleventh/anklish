55a193527318575db3fb85444ff7ad6447aaf408
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2023-05-06 13:42:50 +0400

    + scalafmt

--- src/main/scala/ru/eleventh/anklish/Main.scala
+++ src/main/scala/ru/eleventh/anklish/Main.scala
@@ -8,24 +8,28 @@ import org.slf4j.LoggerFactory
 
 object Main extends IOApp.Simple {
 
-  private val URL = uri"https://api.dictionaryapi.dev/api/v2/entries/en"
+  private val URL                = uri"https://api.dictionaryapi.dev/api/v2/entries/en"
   private val client: Client[IO] = JavaNetClientBuilder[IO].create
-  private val logger = LoggerFactory.getLogger("whatever")
+  private val logger             = LoggerFactory.getLogger("whatever")
 
   private def getDefinition(word: String): IO[DictResponse] = {
-    client.expect[List[DictResponse]](s"$URL/$word").map(_.head).onError(e => {
-      logger.error(e.getMessage)
-      IO(())
-    })
+    client
+      .expect[List[DictResponse]](s"$URL/$word")
+      .map(_.head)
+      .onError(e => {
+        logger.error(e.getMessage)
+        IO(())
+      })
   }
 
   def run: IO[Unit] = {
     Seq("hello", "music", "violin", "cutter", "kek")
       .map(word => {
-        getDefinition(word).map(
-          w => logger.info(s"$word: ${w.meanings.head.definitions.head.definition.get}")
+        getDefinition(word).map(w =>
+          logger.info(s"$word: ${w.meanings.head.definitions.head.definition.get}")
         )
-      }).foreach(io => io.unsafeRunSync()(runtime))
+      })
+      .foreach(io => io.unsafeRunSync()(runtime))
 
     IO(())
   }
