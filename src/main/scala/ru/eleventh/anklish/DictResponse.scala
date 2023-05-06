63bc608d1a411b18da4c9102cdf0027dd4ca8092
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2023-05-06 13:19:42 +0400

    + logging + getting words from the dictionary (draft)

---
+++ src/main/scala/ru/eleventh/anklish/DictResponse.scala
@@ -0,0 +1,17 @@
+package ru.eleventh.anklish
+
+import io.circe.generic.JsonCodec
+
+@JsonCodec
+final case class DictResponse(word: String, phonetics: List[Phonetic], meanings: List[Meaning])
+
+@JsonCodec
+final case class Phonetic(text: Option[String])
+
+@JsonCodec
+final case class Meaning(partOfSpeech: Option[String], definitions: List[Definition])
+
+@JsonCodec
+final case class Definition(definition: Option[String], example: Option[String], synonyms: List[String])
+
+
