d1de3e0aa80f57f379a98633573073e56ceff79b
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2023-05-19 08:47:27 +0600

    ~ IO flow ~ retries

--- src/main/scala/ru/eleventh/anklish/Const.scala
+++ src/main/scala/ru/eleventh/anklish/Const.scala
@@ -5,12 +5,13 @@ import org.http4s.implicits.http4sLiteralsSyntax
 import scala.concurrent.duration.{DurationDouble, FiniteDuration}
 
 object Const {
-  val NET_DICT_API_URL          = uri"https://api.dictionaryapi.dev/api/v2/entries/en"
-  val NET_RETRIES               = 3
-  val NET_DELAY: FiniteDuration = 0.5.second
+  val NET_DICT_API_URL = uri"https://api.dictionaryapi.dev/api/v2/entries/en"
+  val NET_RETRIES = 3
+  val NET_DELAY: FiniteDuration = 1.second
 
-  val ARG_MAX_CARDS = 3
+  val ARG_MAX_CARDS = 10
   val ARG_ANKI_PATH = "anki"
 
-  val ANKI_CONNECT_URL = "localhost:8765"
+  val ANKI_CONNECT_URL = "http://localhost:8765"
+  val ANKI_CONNECT_VERSION = 6
 }
