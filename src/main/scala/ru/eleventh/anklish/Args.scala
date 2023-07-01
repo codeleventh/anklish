a868e09878af34f50e7d90480697286a6ca616b0
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2023-07-01 18:00:52 +0500

    ~ args

--- src/main/scala/ru/eleventh/anklish/Args.scala
+++ src/main/scala/ru/eleventh/anklish/Args.scala
@@ -15,27 +15,28 @@ object Args {
       programName("anklish"),
       head("Scala script for adding English words to Anki decks"),
       head("Check the GitHub repository for more info: https://github.com/codeleventh/anklish"),
-      opt[String]("deckName")
+      opt[String]("deck")
         .abbr("d")
-        .action((x, c) => c.copy(deckName = Some(x)))
+        .action((x, c) => c.copy(deck = Some(x)))
         .optional()
         .maxOccurs(1)
         .text(
           "Anki deck for adding the cards. If no deck specified, the default deck will be used"
         ),
       opt[String]("anki-binary-path")
+        .abbr("anki")
         .action((x, c) => c.copy(ankiBinaryPath = x))
         .optional()
         .maxOccurs(1)
         .text(
-          "Path to Anki binary. It will be triggered to run if Anki Connect port will not respond"
+          s"Path to Anki binary. It will be triggered to run if Anki Connect port will not respond (\"$ARG_ANKI_BINARY_PATH\" as default)"
         ),
       opt[Int]("max-cards-to-add")
         .action((x, c) => c.copy(maxCardsToAdd = x))
         .optional()
         .maxOccurs(1)
         .text(
-          s"The maximum number of cards that should be added to the deck. $ARG_MAX_CARDS as default"
+          s"The maximum number of cards that should be added to the deck ($ARG_MAX_CARDS as default)"
         ),
       opt[Int]("max-unlearned-cards")
         .abbr("max")
@@ -43,7 +44,7 @@ object Args {
         .optional()
         .maxOccurs(1)
         .text(
-          "The number of unlearned cards in the deck, which should not be exceeded (taking into account the existing ones).\nThis parameter (if it is less) will take precedence over the previous one"
+          "The number of unlearned cards in the deck, which should not be exceeded (taking into account the existing ones).\nThis parameter (if it isn't greater) will take precedence over the previous one"
         ),
       arg[File]("<input_file>")
         .required()
@@ -58,7 +59,7 @@ object Args {
       maxUnlearnedCards: Option[Int] = None,
       maxCardsToAdd: Int = ARG_MAX_CARDS,
       ankiBinaryPath: String = ARG_ANKI_BINARY_PATH,
-      deckName: Option[String] = None,
+      deck: Option[String] = None,
       file: Option[File] = None
   )
 }
