834c8805cde61f94befb613bf37fbe9e0235d561
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2023-07-01 11:37:54 +0500

    ~ core logic ~ error handling ~ args ~ logging

--- src/main/scala/ru/eleventh/anklish/Const.scala
+++ src/main/scala/ru/eleventh/anklish/Const.scala
@@ -5,13 +5,15 @@ import org.http4s.implicits.http4sLiteralsSyntax
 import scala.concurrent.duration.{DurationDouble, FiniteDuration}
 
 object Const {
-  val NET_DICT_API_URL = uri"https://api.dictionaryapi.dev/api/v2/entries/en"
-  val NET_RETRIES = 3
-  val NET_DELAY: FiniteDuration = 1.second
+  val NET_DICT_API_URL               = uri"https://api.dictionaryapi.dev/api/v2/entries/en"
+  val NET_RETRIES                    = 3
+  val NET_WAIT_RETRY: FiniteDuration = 1.second
 
   val ARG_MAX_CARDS = 10
   val ARG_ANKI_BINARY_PATH = "anki"
 
-  val ANKI_CONNECT_URL = "http://localhost:8765"
+  val ANKI_CONNECT_URL     = "http://localhost:8765"
   val ANKI_CONNECT_VERSION = 6
+
+  val DEFAULT_DECK_ID = 1L
 }
---
+++ src/main/scala/ru/eleventh/anklish/HttpClient.scala
@@ -0,0 +1,38 @@
+package ru.eleventh.anklish
+
+import cats.effect.IO
+import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
+import org.http4s.client.{Client, JavaNetClientBuilder}
+import org.slf4j.{Logger, LoggerFactory}
+import ru.eleventh.anklish.Const.{NET_DICT_API_URL, NET_WAIT_RETRY}
+import ru.eleventh.anklish.model.DictResponse
+
+import java.net.URLEncoder
+
+object HttpClient {
+
+  implicit val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
+  implicit val logger: Logger         = LoggerFactory.getLogger(this.getClass)
+
+  def getDefinition(word: String, retries: Int): IO[Either[Throwable, DictResponse]] = {
+    httpClient
+      .expect[List[DictResponse]](s"$NET_DICT_API_URL/${URLEncoder.encode(word, "UTF-8")}")
+      .attempt
+      .map {
+        case Right(dictResponse) => Right(dictResponse.head)
+        case Left(e)             => Left(e)
+      }
+      .flatMap {
+        case suc => IO(suc)
+        case Left(e) =>
+          retries match {
+            case 0 => IO(Left(e))
+            case _ => IO.sleep(NET_WAIT_RETRY) *> getDefinition(word, retries - 1)
+          }
+      }
+      .flatTap {
+        case Left(e)  => IO(logger.error(s"Word \"$word\": ${e.getMessage}"))
+        case Right(_) => IO(logger.info(s"Got definition for \"$word\""))
+      }
+  }
+}
